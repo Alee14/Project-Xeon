22fe6650988ab9e3f3e301177f2482df6c92b711
AdrUlb <adrian.ulbrich.2003@gmail.com>
2019-01-28 21:30:41 +0100

    Updated MSBuild script

--- build-msbuild.bat
+++ build-msbuild.bat
@@ -1,37 +1,81 @@
 @echo off
 title Building C++ Program
 
+:findMsBuild
 set msBuildPath=0
-
-if exist "C:\Program Files (x86)\MSBuild\12.0\Bin" (
-  set msBuildPath="C:\Program Files (x86)\MSBuild\12.0\Bin"
-  set "VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120"
-) else (
-  if exist "C:\Program Files\MSBuild\12.0\Bin" (
-    set msBuildPath="C:\Program Files\MSBuild\12.0\Bin"
-	set "VCTargetsPath=C:\Program Files\MSBuild\Microsoft.Cpp\v4.0\V120"
-  )
+if exist "C:\Program Files\MSBuild" (
+  set "msBuildPath=C:\Program Files\MSBuild"
 )
-
-if exist "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120" (
-  set toolVersion=v120
-) else (
-  if exist "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110" (
-    set toolVersion=v110
-  )
+if exist "C:\Program Files (x86)\MSBuild" (
+  set "msBuildPath=C:\Program Files (x86)\MSBuild"
 )
-
-
-if not %msBuildPath%==0 (
-  REM set VCTargetsPath=%msBuildPath%
-  echo Building Program...
-  %msBuildPath%\msbuild xeon.vcxproj /p:outdir=out /p:assemblyname=game /p:configuration=release 
-  echo Building Complete!
-  pause > nul
+if "%msBuildPath%"=="0" (
+  echo MSBuild could not be found. Please make sure that MSBuild is installed.
+  pause>nul
   exit
+)
+
+:findMsBuildVersion
+set msBuildVersion=0
+if exist "%msBuildPath%\12.0" (
+  set msBuildVersion=12.0
+)
+if %msBuildVersion%==0 goto manualMsBuildVersionOverride
+
+:findTools
+echo ----------------------------------------
+echo Found MSBuild version %msBuildVersion%
+echo ----------------------------------------
+
+set toolVersion=0
+if exist "%msBuildPath%\Microsoft.Cpp\v4.0\V110" (
+  set toolVersion=v110
+)
+if exist "%msBuildPath%\Microsoft.Cpp\v4.0\V120" (
+  set toolVersion=v120
+)
+if "%toolVersion%"=="0" goto manualToolVersionOverride
+
+:setVCTargets
+echo ----------------------------------------
+echo Found VCTools version %toolVersion%
+echo ----------------------------------------
+
+set "VCTargetsPath=%msBuildPath%\Microsoft.Cpp\v4.0\%toolVersion%"
+
+:build
+echo Building Program...
+"%msBuildPath%\%msBuildVersion%\Bin\msbuild.exe" xeon.vcxproj /p:outdir=out /p:assemblyname=game /p:configuration=release > msbuild.log
+echo Building Complete!
+pause>nul
+exit
+
+
+
+:manualMsBuildVersionOverride
+echo Could not find MSBuild 12.0
+echo Please specify the installed version manually or close this window,
+echo install either of these versions and try again.
+set /p manualMsBuildVersion=MSBuild version number: 
+set msBuildVersion=%manualmsBuildVersion%
+if exist "%msBuildPath%\%manualMsBuildVersion%" (
+  goto findTools
 ) else (
-  echo ERROR: Could not find MSBuild
-  echo Unable to build Program using MSBuild!
-  pause > nul
+  echo Could not find MSBuild version!
+  pause>nul 
+  exit
+)
+
+:manualToolVersionOverride
+echo Could not find MSBuild tools V120 or V110.
+echo Please specify the version of installed tools manually
+echo or close this window, install either of these versions and try again.
+set /p manualToolVersion=Tool version number: 
+set toolVersion=v%manualToolVersion%
+if exist "%msBuildPath%\Microsoft.Cpp\v4.0\%toolVersion%" (
+  goto setVCTargets
+) else (
+  echo Could not find tool version!
+  pause>nul
   exit
 )
