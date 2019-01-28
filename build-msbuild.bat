8d1b74dd3b1d04c66fd6da0c3220e65e86f554bd
AdrUlb <adrian.ulbrich.2003@gmail.com>
2019-01-28 19:35:46 +0100

    Extended MSBuild Support

--- build-msbuild.bat
+++ build-msbuild.bat
@@ -6,23 +6,31 @@ set msBuildPath=0
 if exist "C:\Program Files (x86)\MSBuild\12.0\Bin" (
   set msBuildPath="C:\Program Files (x86)\MSBuild\12.0\Bin"
   set "VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120"
-)
-else (
-if exist "C:\Program Files\MSBuild\12.0\Bin" (
+) else (
+  if exist "C:\Program Files\MSBuild\12.0\Bin" (
     set msBuildPath="C:\Program Files\MSBuild\12.0\Bin"
 	set "VCTargetsPath=C:\Program Files\MSBuild\Microsoft.Cpp\v4.0\V120"
   )
 )
+
+if exist "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120" (
+  set toolVersion=v120
+) else (
+  if exist "C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110" (
+    set toolVersion=v110
+  )
+)
+
 
 if not %msBuildPath%==0 (
   REM set VCTargetsPath=%msBuildPath%
   echo Building Program...
   %msBuildPath%\msbuild xeon.vcxproj /p:outdir=out /p:assemblyname=game /p:configuration=release 
   echo Building Complete!
-  pause>nul
+  pause > nul
   exit
 ) else (
-  echo ERROR: Could not find MSBuild 12.0
+  echo ERROR: Could not find MSBuild
   echo Unable to build Program using MSBuild!
   pause > nul
   exit
